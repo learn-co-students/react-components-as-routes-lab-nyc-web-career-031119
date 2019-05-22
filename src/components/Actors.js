66addcd11c15c4f14a20172891c9a44dbd67d519
joshuamsmith413 <joshuamsmith413@gmail.com>
2019-05-22 17:23:10 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,9 +2,23 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+
+  const actorMovies = arr => {
+    return arr.map(movie => {
+      return <li>{movie}</li>
+    })
+  }
+
+  const actorsName = arr => {
+    return arr.map(actor => {
+      return <div><h1>{actor.name}</h1><ul>{actorMovies(actor.movies)}</ul></div>
+    })
+  }
+
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actorsName(actors)}
     </div>
   );
 };
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,9 +2,23 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
+console.log(directors)
+  const directorMovies = arr => {
+    return arr.map(movie => {
+      return <li>{movie}</li>
+    })
+  }
+
+  const directorsName = arr => {
+    return arr.map(director => {
+      return <div><h1>{director.name}</h1><ul>{directorMovies(director.movies)}</ul></div>
+    })
+  }
+
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directorsName(directors)}
     </div>
   );
 }
